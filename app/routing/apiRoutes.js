853ed9ce338215151543ee69a7869f9ecedb33d3
druchefavour <druchefavour@gmail.com>
2017-02-11 19:22:19 -0600

    commit debugged apiRoute.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,9 +7,9 @@ var http = require("https");
 
 // ==================================
 module.exports = function(app) {
-
+//999999999999999999999999999999999999999999999999999999999999999999999999999
 	// When the page loads, grab all our friends
-	$.get("/api/all", function(data) {
+	app.get("/api/all", function(data) {
 		if (data.length !== 0) {
 			for (var i = 0; i < data.length; i++) {
 				var profile = $("<div>");
@@ -18,17 +18,17 @@ module.exports = function(app) {
 				profile.append("<p>" + data[i].photo +"<p>");
 				profile.append("<p>" + data[i].score +"<p>");
 				$("friends-rep").prepend(profile);
-			
-	// Make Basic Calculate comparison
-	var friendScore[i] = data[i].score;
-	if (data.length > 1) {
-		for (var i = 0; i < data.length-1, i++) {
-			for (var j = 0; j < friendScore.length, j++){
-				var friendComp[i][j] = friendScore[data.length][j] - friendScore[i][j]; 
-				var friendAbs[i][j] = math.abs(friendComp[i][j]);
-				var friendSum[i] = sum([friendAbs[i][1], friendAbs[i][2], friendAbs[i][3], friendAbs[i][4], friendAbs[i][5], friendAbs[i][6], friendAbs[i][7], friendAbs[i][8], friendAs[i][9], friendAbs[i][10]]);
-				var friendSum = [];
-				friendSum.push(friendSum[i])
+			// Make Basic Calculate comparison
+			var friendScore = [];
+
+			data[i].score = [data[i].score[0], data[i].score[1],data[i].score[2], data[i].score[3], data[i].score[4], data[i].score[5], data[i].score[6], data[i].score[7], data[i].score[8], data[i].score[9]];
+
+			var friendSum = [];
+			friendSum[i] = sum([math.abs((data[length].score[0])-(data[i].score[0])), math.abs((data[length].score[1])-(data[i].score[1])), math.abs((data[length].score[2])-(data[i].score[2])), math.abs((data[length].score[3])-(data[i].score[3])),math.abs((data[length].score[4])-(data[i].score[4])), math.abs((data[length].score[5])-(data[i].score[5])), math.abs((data[length].score[6])-(data[i].score[6])), math.abs((data[length].score[7])-(data[i].score[7])), math.abs((data[length].score[8])-(data[i].score[8])), math.abs((data[length].score[9])-(data[i].score[9]))]);
+
+				friendSum.push(friendSum[i]);
+				//0000000000000000000000000000000000000000000000000
+				{
 					if(friendSum[i] === Math.min.apply(null, friendSum)){
 					// Grab the result from the AJAX post so that the best match's name and photo are displayed.
 					$("#bestMatchName").text(data[i].name);
@@ -39,9 +39,10 @@ module.exports = function(app) {
 						console.log("start afresh");
 					}
 				}
+				//00000000000000000000000000000000000000000000000000000
 			}
 			}
-		}
+		})
+	//99999999999999999999999999999999999999999999999999999999999999999
 	};
-});
-}
+
